278f92d131fee5d4bca8d1f651925f5dc968de17
mangel83 <mangel83@gmail.com>
2020-09-21 15:23:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7tgrn9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7tgrn9)
---
+++ src/app/datepicker-color-example.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/** @title Datepicker palette colors */
+@Component({
+  selector: 'datepicker-color-example',
+  templateUrl: 'datepicker-color-example.html',
+  styleUrls: ['datepicker-color-example.css'],
+})
+export class DatepickerColorExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
